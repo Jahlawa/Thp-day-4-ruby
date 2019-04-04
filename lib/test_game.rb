100c7ad0a9f04234280b5e8e3ec9413881fe0d60
Jahlawa <48957615+Jahlawa@users.noreply.github.com>
2019-04-04 22:03:38 +0200

    Update test_game.rb

--- lib/test_game.rb
+++ lib/test_game.rb
@@ -6,22 +6,22 @@ while x<=9 && x>=0
 	y=rand(1...6)
 	if (y==5) || (y==6)
 		x+=1
-		puts "tu as fiat#{y}=> Tu as bien monte un etage; tu es au #{x}eme"
+		puts "tu as fait#{y}=> Tu as bien monte un etage; tu es au #{x}eme"
 	elsif (y==1) && (x<=0)
 		if x>0 
 		x=-1
-		puts "tu as fiat#{y}=> Tu es decendu d\'un etage; tu es au #{x}eme"
+		puts "tu as fait#{y}=> Tu es descendu d\'un etage; tu es au #{x}eme"
 		end  
-		puts "tu est tout en bas, fait un peu d'effort"
+		puts "tu es tout en bas, fait un peu d'effort"
 		next 
 	else
-		puts "tu as fiat#{y}=> Tu bouge pas, donc tu es encore au #{x}eme"
+		puts "tu as fait#{y}=> Tu bouge pas, donc tu es encore au #{x}eme"
 		x+=0
 	end
 		z+=1
 end   
 puts "Bravo tu es au 10eme etage"
-puts " tu as fait #{z} fois d'essaie"
+puts " tu as fait #{z} fois d'essais"
 end 
 
 jeux_de_roi
